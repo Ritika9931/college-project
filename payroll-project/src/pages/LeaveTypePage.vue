2d591a373b3b933264b9a86c35bcff2cebb08f07
Ritika <kumariritikak2000@gmail.com>
2024-03-01 20:13:52 -0800

    first

--- payroll-project/src/pages/LeaveTypePage.vue
+++ payroll-project/src/pages/LeaveTypePage.vue
@@ -1,49 +1,27 @@
 <template>
-    <q-page class="q-pa-md ">
-        <div class="column items-center q-my-md">
-            <div class="text-h6  text-black-3"> New leave</div>
-            
-            <q-card class="q-pa-md q-mt-md" style="width:380px">
-                
-        <div class="row q-pa-sm">
-            <div class="coloumn col" style="margin-right: 20px;" >
-                <label for="type">Type</label>
-                <q-input outlined v-model="text"/></div>
-        </div> 
-        <div class="row q-pa-sm">
-            <div class="coloumn col" style="margin-right: 20px;" >
-                <label for="cause">Cause</label>
-                <q-input outlined v-model="text"/></div>
-        </div> 
-        <div class="row q-pa-sm">
-            <div class="coloumn col" style="margin-right: 20px;" >
-                <label for="from">From</label>
-                <q-input outlined v-model="text" /></div>
-            </div> 
-            <div class="q-pa-md">
-    <div class="q-pb-sm">
-      Model: {{ days }}
+    
+    <q-page class="q-pa-md bg-grey-2">
+   
+   
+    <div><q-btn label="Add" to="./add" color="green" rounded></q-btn></div>
+    <div class="row q-gutter-md q-my-md">
+        <q-card v-if="mode" class="q-pa-md" style="width:360px">
+      <router-view/> 
+    </q-card>
+    
+    <LeaveTypeTable class="col"/>
     </div>
 
-    <q-date v-model="days" multiple />
-  </div> 
+ 
 
-    
-        <div class="row q-pa-sm">
-            <div class="coloumn col" style="margin-right: 20px;" >
-                <label for="to">To</label>
-                <q-input outlined v-model="text" /></div>
-                  
-        </div>
-        <q-btn color="primary" label="APPLY" />
-        
-        </q-card>
-    </div>
-           
-    </q-page>
-   </template>
-    <script>
-    export default {
-    
-    }
-    </script>
+</q-page>
+</template>
+
+<script>
+import LeaveTypeTable from 'components/modules/leavetypes/table.vue'
+export default {
+props: ['mode'],
+    components: {LeaveTypeTable,}
+
+}
+</script>
