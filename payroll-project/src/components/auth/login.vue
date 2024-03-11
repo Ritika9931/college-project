4969ffb238ce4c203601d6e22f1aed6e4c348fd6
Ritika <kumariritikak2000@gmail.com>
2024-03-10 17:38:47 -0700

    login

--- payroll-project/src/components/auth/login.vue
+++ payroll-project/src/components/auth/login.vue
@@ -1,32 +1,29 @@
 <template>
 
-  <div class="column items-center q-my-md">
-    <div class="text-h5 text-bold text-black">Log In</div>
-
-    <q-form class="q-pa-md q-mt-md bg-white" style="width:400px">
-
-      <div class="row q-pa-sm">
-        <div class="coloumn col" style="margin-right: 20px;">
-          <label for="email">Login Id</label>
-          <q-input outlined v-model="auth.email" Placeholder="e.g.riti123@gmail.com" />
-        </div>
-
+  <div class="column items-center">
+    
+ <q-form class="q-pa-md q-mt-md bg-white" style="width:400px">
+      <q-card class="q-pa-md shadow-2 my_card" bordered>
+        <div>
+      
+      <h4 class="text-center text-weight-bold text-black">LOGIN</h4>
+      <q-input outlined label="Email" v-model="auth.email" />
+      <q-input outlined label="Password" v-model="auth.password" />
+      
       </div>
-      <div class="row q-pa-sm">
-        <div class="coloumn col" style="margin-right: 20px;">
-          <label for="password">Password</label>
-          <q-input outlined v-model="auth.password" Placeholder="e.g.23@3$" />
-        </div>
-      </div>
-      <div><q-btn label="LogIn" color="blue" rounded @click="login"></q-btn></div>
-
-
+      
+      <div class="full-width q-my-md column q-gutter-sm">
+        <q-checkbox v-model="check" label="Remember Me"></q-checkbox>
+        <div class="">
+      <q-btn :label=" authSucess? 'Success':'LOGIN'"  class="full-width"
+      :color="authSucess?'green':'primary'" @click="login" :disabled="authInProgress||authSuccess"
+      :loading="authInProgress"></q-btn>
+    </div>
+    </div>
+    </q-card>
     </q-form>
 
   </div>
-
-
-
 </template>
 <script>
 
@@ -35,21 +32,40 @@ export default {
   data () {
     return {
       auth: {},
-
-
+      authInProgress: false,
+      authSuccess: false
     }
   },
   methods: {
     async login () {
-      let httpRequest = await this.$axios.post('http://localhost:8055/auth/login', this.auth)
-      console.log(httpRequest)
+      this.$axios.defaults.headers.common['Authorization'] = null;
+      this.authInProgress = true
+      let httpRequest
+      try {
+        httpRequest = await this.$api.post('/auth/login', this.auth)
+      } catch (err) {
+        console.log(err)
+        if (err.response.status === 401) {
+          this.$q.dialog({
+            message: err?.response?.data?.errors?.[0]?.message
+          })
+        }
+        this.authInProgress = false
+        return
+
+      }
+      this.authSuccess = true
+      this.authInProgress = false
       let access_token = httpRequest.data.data.access_token
-      this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token;
+      this.$api.defaults.headers.common['Authorization'] = 'Bearer ' + access_token;
       localStorage.setItem('access_token', access_token)
-    }
 
-
+      this.$mitt.emit('login-successfull')
+      setTimeout(() => {
+        this.$router.replace('/')
+      }, 1000)
   }
+}
 }
 
 
