89300941cd08d06f49618310e73307979ab755da
Rakesh Roshan <ricky.it.expert@gmail.com>
2024-03-24 10:12:48 +0530

    attendance work WIP

--- directus-backend/extensions/endpoints/time/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function register (router, { services, exceptions, logger }) {
-  router.get('/getCurrentTime', function (req, res) {
-    res.send('Time Server')
-  })
-}
--- payroll-project/src/components/modules/attendence/markAttendance.vue
+++ payroll-project/src/components/modules/attendence/markAttendance.vue
@@ -3,17 +3,22 @@
     <q-btn label="Attendance" color="green" @click="openDialog = true"></q-btn>
 
     <q-dialog v-model="openDialog">
-      <q-card class="q-pa-md">
-        <div class="text-h5 q-my-md ">Mark Attendance</div>
-        <div v-if="checkedout" class="column q-gutter-md">
-          {{ location }}
+      <q-card>
+        <div class="text-h5 bg-primary q-pa-md text-white">Mark Attendance</div>
+        <div class="column q-gutter-md q-pa-md">
+          <div>
+            <div class="text-h6">{{ employee.name }} - {{ employee.employee_code }}</div>
+            <div class="text-grey">{{ employee.designation_id.name }}</div>
+          </div>
+
           <div class="text-red" v-if="locationErrorMessage">{{ locationErrorMessage }}: Please allow permission to
             continue</div>
-          <div>Last checkin : 18 March 2024 09:30am</div>
-          <div>Last checkout: 18 March 2024 06:00am</div>
+          <div v-if="checkedInTime">Last checkin : {{ checkedInTime }}</div>
+          <div v-if="checkedOutTime">Last checkout: {{ checkedOutTime }}</div>
           <div class="row">
-            <q-btn label="CheckIn" color="green" v-if="checkedout" :disabled="locationErrorMessage"></q-btn>
-            <q-btn label="CheckOut" color="red" v-if="checkedin"></q-btn>
+            <q-btn label="CheckIn" color="green" v-if="!checkedInTime" @click="markAttendance"
+              :disabled="locationErrorMessage"></q-btn>
+            <q-btn label="CheckOut" color="red" v-if="checkedInTime && !checkedOutTime" @click="markAttendance"></q-btn>
             <q-btn label="History" color="grey" flat></q-btn>
           </div>
         </div>
@@ -22,16 +27,79 @@
   </div>
 </template>
 <script>
+import moment from 'moment'
 export default {
   data () {
     return {
-      checkedout: true,
       openDialog: false,
       location: {},
-      locationErrorMessage: null
+      locationErrorMessage: null,
+      employee: null,
+      attendances: []
+    }
+  },
+  computed: {
+    checkedInTime () {
+      let checkedInTime = false
+      if (this.attendances?.[0]) {
+        checkedInTime = moment(this.attendances?.[0].log_time).format('DD-MMM-YY hh:mm')
+      }
+      return checkedInTime
+    },
+    checkedOutTime () {
+      let checkedOutTime = false
+      if (this.attendances.length > 1) {
+        let checkout = this.attendances[this.attendances.length - 1]
+        checkedOutTime = moment(checkout.log_time).format('DD-MMM-YY hh:mm')
+      }
+      return checkedOutTime
     }
   },
   methods: {
+    async fetchAttendances () {
+      console.log(moment)
+      let today = moment().format('YYYY-MM-DD')
+      let httpClient = await this.$api.get('/items/attendences', {
+        params: {
+          filter: {
+            log_time: {
+              _gt: today
+            },
+            employee_id: {
+              _eq: this.employee.id
+            }
+          }
+        }
+      })
+      this.attendances = httpClient.data.data
+    },
+    async markAttendance () {
+      let httpClient = await this.$api.post('/items/attendences', {
+        employee_id: this.employee.id,
+        log_source: 'in_app',
+        location: {
+          type: 'Point',
+          coordinates: [this.location.longitude, this.location.latitude]
+        }
+      })
+      this.fetchAttendances()
+    },
+    async fetchEmployee () {
+      let httpClient = await this.$api.get('users/me')
+      let profile = httpClient.data.data
+      let httpClient2 = await this.$api.get('items/employees', {
+        params: {
+          fields: ['*', 'designation_id.*'],
+          filter: {
+            user_id: {
+              _eq: profile.id
+            }
+          }
+        }
+      })
+      this.employee = httpClient2?.data?.data?.[0]
+      this.fetchAttendances()
+    },
     getLocation () {
       console.log(navigator)
       let self = this
@@ -50,6 +118,8 @@ export default {
   },
   created () {
     this.getLocation()
+    this.fetchEmployee()
+
   }
 }
 </script>
